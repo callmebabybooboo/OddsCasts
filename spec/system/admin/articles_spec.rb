d8cb607caec46051425d5daf7532d88db8cfac0a
callmebabybooboo <napwan.lua@gmail.com>
2024-12-09 14:38:46 +0700

    update rspec article_spec

--- spec/system/admin/articles_spec.rb
+++ spec/system/admin/articles_spec.rb
@@ -10,22 +10,22 @@ describe "Articles management", type: :system do
         # user enters admin page
         visit admin_articles_path
         # user want to create new article
-        click_button("Create New Article")
+        click_link("Create New Article")
         # fill in article detail
-        fill_in("title", with: "RubyOnRails")
-        fill_in("description", with: "Rails is a full-stack framework. It ships with all the tools needed to build amazing web apps on both the front and back end.")
-        fill_in("cover_url", with: "https://encrypted-tbn2.gstatic.com/images?q=tbn:ANd9GcTWdUeac3VidKruvmUTcqewnVdh_XEjvrrnxQujrVjtugyMoWJawSiLWMBmUEDwd9AGzyzQxTpA5xKGyd7cPeqeBcvJ1dsnBjlaN5insxw")
-        # click button create
+        fill_in("article[title]", with: "RubyOnRails")
+        fill_in("article[description]", with: "Rails is a full-stack framework. It ships with all the tools needed to build amazing web apps on both the front and back end.")
+        fill_in("article[cover_url]", with: "https://encrypted-tbn2.gstatic.com/images?q=tbn:ANd9GcTWdUeac3VidKruvmUTcqewnVdh_XEjvrrnxQujrVjtugyMoWJawSiLWMBmUEDwd9AGzyzQxTpA5xKGyd7cPeqeBcvJ1dsnBjlaN5insxw")
+        # user create the article
         click_button("Create Article")
-        # user should see an article
-        expect(page).to have_content("Article was successfully create.")
-        expect(page).to have_content("New Article")
+        # # user should see an article
+        expect(page).to have_content(article.title)
+        expect(page).to have_content(article.description)
     end
 
     it "lists article" do
         # user enters admin page
-        # visit admin_articles_path
-        # # user should see a list of articles
+        visit admin_articles_path
+        # user should see a list of articles
         # expect(page).to have_content(article.title)
         # expect(page).to have_content(article.description)
     end
